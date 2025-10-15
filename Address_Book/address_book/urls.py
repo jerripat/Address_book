d3880f59f63220607e870609ac9682dfcc0172c7
jerripat <jerripatterson@gmail.com>
2025-10-14 19:59:13 -0500

    semi working

--- Address_Book/address_book/urls.py
+++ Address_Book/address_book/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'address_book'
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.add_address, name='home'),           # ✅ new: makes "/" work
+    path('add_address/', views.add_address, name='add_address'),
 ]
